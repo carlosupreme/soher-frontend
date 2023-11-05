cdbda0fcd094f32d0b8d490c75cc24d41dbff433
Carlos Sosa <carlos.alberto.sosa.perera@gmail.com>
2023-11-04 22:50:39 -0600

    consuming first endpoint

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import axios from "axios";
+
+function App() {
+  const [works, setWorks] = useState([]);
+  useEffect(() => {
+    axios.get("http://127.0.0.1:8000/api/works").then((res) => {
+      setWorks(res.data.data);
+    });
+  }, []);
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        {works.map((work) => {
+          return (
+            <div key={work.id} style={{ border: "1px solid red" }}>
+              <h1>{work.id}</h1>
+              <h2>{work.title}</h2>
+              <h3>Solicitado por: {work.client.name}</h3>
+              <h4>{work.created_at}</h4>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+}
+
+export default App;
